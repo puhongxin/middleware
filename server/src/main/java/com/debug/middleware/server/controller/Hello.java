db7e86da60cdea01d3fcc0d25bb6785d777edadd
xiaopu <1370230564@qq.com>
2020-11-30 20:17:28 +0800

    我添加了个输出

--- server/src/main/java/com/debug/middleware/server/controller/Hello.java
+++ server/src/main/java/com/debug/middleware/server/controller/Hello.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Hello {
     @RequestMapping("/hello")
     public String hello(){
+        System.out.println("123");
         return "123";
     }
 }
